6157954e27f19aee923c1dcede94c4665e52d409
hatrari <ham.atrari@gmail.com>
2020-11-24 11:44:02 +0100

    add product and category models

---
+++ ProductService/Models/Category.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+namespace ProductService.Models
+{    
+  public class Category
+  {
+    public Category()
+    {
+      Products = new List<Product>();
+    }
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public IEnumerable<Product> Products { get; set; }
+  }
+}
